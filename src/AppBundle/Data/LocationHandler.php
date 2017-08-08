22a776695ccb6fbfd62db875f7af7031087d23f0
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-08-08 22:49:03 +0200

    Making database more speedy

--- src/AppBundle/Data/LocationHandler.php
+++ src/AppBundle/Data/LocationHandler.php
@@ -93,7 +93,7 @@ class LocationHandler {
             ->sort('date', 'asc')
             ->getQuery();
 
-//        $query->useResultCache(true, 60, $personName . '_route_id')
+        $query->useResultCache(true, 60, $personName . '_route_id')
 
         $iterableResult = $query->iterate();
 
